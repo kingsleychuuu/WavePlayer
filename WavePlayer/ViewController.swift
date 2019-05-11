a7a358c871378e62e62809c310c2faa9326fa42f
Kingsley Chu <kingsleychu0730@hotmail.com>
2019-05-11 05:22:08 -0400

    Initial Commit

---
+++ WavePlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WavePlayer
+//
+//  Created by admin on 5/11/19.
+//  Copyright © 2019 Kingsley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
