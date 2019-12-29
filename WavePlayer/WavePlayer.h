3a2113327d776c9fc5895343e37d765327057175
Kingsley Chu <kingsleychu0730@hotmail.com>
2019-12-29 01:24:10 -0500

    Integrated Spotify frameworks

---
+++ WavePlayer/WavePlayer.h
@@ -0,0 +1,16 @@
+//
+//  WavePlayer.h
+//  WavePlayer
+//
+//  Created by admin on 12/29/19.
+//  Copyright © 2019 Kingsley. All rights reserved.
+//
+
+#ifndef WavePlayer_h
+#define WavePlayer_h
+
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
+#import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
+#import <SpotifyMetadata/SpotifyMetadata.h>
+
+#endif /* WavePlayer_h */
